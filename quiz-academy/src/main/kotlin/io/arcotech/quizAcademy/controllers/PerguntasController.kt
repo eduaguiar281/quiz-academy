60275b0684abc405e7676b07ca183b993a89698f
eduaguiar281 <eduaguiar281@hotmail.com>
2023-02-07 16:26:12 -0300

    Melhoria nas Respostas

--- quiz-academy/src/main/kotlin/io/arcotech/quizAcademy/controllers/PerguntasController.kt
+++ quiz-academy/src/main/kotlin/io/arcotech/quizAcademy/controllers/PerguntasController.kt
@@ -4,7 +4,10 @@ import io.arcotech.quizAcademy.dto.AlteraPerguntaForm
 import io.arcotech.quizAcademy.dto.NovaPerguntaForm
 import io.arcotech.quizAcademy.models.Pergunta
 import io.arcotech.quizAcademy.services.PerguntaService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.util.UriComponentsBuilder
 
 @RestController
 @RequestMapping("/perguntas")
@@ -20,16 +23,24 @@ class PerguntasController (val perguntaService: PerguntaService) {
     }
 
     @PostMapping
-    fun cadastrar(@RequestBody form: NovaPerguntaForm){
-        perguntaService.cadastrar(form)
+    fun cadastrar(
+        uriBuilder: UriComponentsBuilder,
+        @RequestBody form: NovaPerguntaForm
+    ): ResponseEntity<Pergunta> {
+        val novaPergunta = perguntaService.cadastrar(form)
+        val uri = uriBuilder.path("/perguntas/${novaPergunta.id}").build().toUri()
+        return ResponseEntity.created(uri).body(novaPergunta)
     }
 
     @PutMapping
-    fun alterar(@RequestBody form: AlteraPerguntaForm){
-        perguntaService.alterar(form)
+    fun alterar(
+        @RequestBody form: AlteraPerguntaForm
+    ):ResponseEntity<Pergunta> {
+        return ResponseEntity.ok(perguntaService.alterar(form))
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     fun excluir(@PathVariable id: Long){
         perguntaService.deletar(id)
     }
