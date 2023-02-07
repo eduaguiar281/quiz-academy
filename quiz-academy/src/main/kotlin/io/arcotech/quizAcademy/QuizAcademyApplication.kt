59a6b6f7d485ea42d4b2556e37407c836e0e5d91
eduaguiar281 <eduaguiar281@hotmail.com>
2023-02-07 13:39:44 -0300

    Primeira Versão

---
+++ quiz-academy/src/main/kotlin/io/arcotech/quizAcademy/QuizAcademyApplication.kt
@@ -0,0 +1,11 @@
+package io.arcotech.quizAcademy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuizAcademyApplication
+
+fun main(args: Array<String>) {
+	runApplication<QuizAcademyApplication>(*args)
+}
---
+++ quiz-academy/src/test/kotlin/io/arcotech/quizAcademy/QuizAcademyApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.arcotech.quizAcademy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QuizAcademyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
